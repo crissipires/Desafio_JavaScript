92663beed4e30f6d6e97653015c90f232a05301b
crissipires <crissiano_pires@hotmail.com>
2022-02-05 14:14:41 -0300

    Add sample

---
+++ questao7.js
@@ -0,0 +1,17 @@
+const calcularImc = function(peso,altura){
+    return (peso / Math.pow(altura, 2)).toFixed(2);
+}
+
+const classificacaoImc = function(peso,altura){
+    let imc = calcularImc(peso,altura);
+
+    if(imc < 18){
+        return "Abaixo do peso";
+    } else if(imc < 25){
+        return "Peso Normal"
+    } else{ 
+        return "Acima do peso"
+    }
+}
+
+console.log(classificacaoImc(80,2.00))
